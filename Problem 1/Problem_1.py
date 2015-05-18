f8339c9e7cc3d5caa9e22f8f1b374d9e1442f2d9
Graeme Bates <BatesG1996@Graemes-MacBook-Pro.local>
2015-05-17 18:19:02 -0700

    Removed .idea/

--- Problem 1/Problem_1.py
+++ Problem 1/Problem_1.py
@@ -1,6 +1,7 @@
 __author__ = 'BatesG1996'
 
 import Problem_1_helpers as p1
+
 def solve_problem_1():
     max = 1000
     multiples_of_five = p1.get_multiples_of(5, max)
---
+++ Problem 3/Problem_3_helpers.py
@@ -0,0 +1,1 @@
+__author__ = 'BatesG1996'
